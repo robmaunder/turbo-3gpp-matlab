023de724a85a6312fe1e433d26112afa3d6e29ff
Rob Maunder <rob.maunder@accelercomm.com>
2019-01-03 22:23:39 +0000

    Added a testbench and a version of the code I wrote several years ago.

---
+++ subblock_interleaver.m
@@ -0,0 +1,31 @@
+function v = subblock_interleaver(d,superscript)
+
+D = length(d);
+
+C_TC_subblock = 32;
+
+R_TC_subblock = ceil(D/C_TC_subblock);
+
+N_D = R_TC_subblock*C_TC_subblock-D;
+
+y = [nan(1,N_D),d];
+
+matrix = reshape(y,C_TC_subblock,R_TC_subblock)';
+
+P = [0 16 8 24 4 20 12 28 2 18 10 26 6 22 14 30 1 17 9 25 5 21 13 29 3 19 11 27 7 23 15 31];
+
+K_PI = R_TC_subblock*C_TC_subblock;
+
+if superscript < 2
+    
+    matrix = matrix(:,P+1);
+    
+    v = reshape(matrix, 1, K_PI);
+    
+else
+    pi = mod(P(floor((0:K_PI-1)/R_TC_subblock)+1) + C_TC_subblock*mod(0:K_PI-1,R_TC_subblock)+1, K_PI);
+    
+    v = y(pi+1);
+end
+
+end
---
+++ test_subblock_interleaver.m
@@ -0,0 +1,42 @@
+while 1
+    
+    D = randi(6148);
+    
+    d = 0:D-1;
+    
+    subblock_interleaver_index = randi([0,2]);
+    
+    fprintf('%d %d\n',D,subblock_interleaver_index);
+    
+    v1 = subblock_interleaver(d,subblock_interleaver_index);
+    
+    pi = get_3gpp_subblock_interleaver(D,subblock_interleaver_index);
+    
+    v2 = zeros(size(pi));
+    v2(~isnan(pi)) = d(pi(~isnan(pi))+1);
+    v2(isnan(pi)) = nan;
+    
+    d2 = zeros(1,D);
+    d2(pi(~isnan(pi))+1) = v2(~isnan(pi));
+    
+    if ~isequal(d,d2)
+        [d;d2]
+        error('Rob3');
+    end
+    
+    
+    
+    
+    
+    if ~isequal(isnan(v1),isnan(v2))
+        [v1;v2]
+        error('Rob');
+    end
+    
+    if ~isequal(v1(~isnan(v1)),v2(~isnan(v2)))
+        [v1;v2]
+        error('Rob2');
+    end
+    
+    
+end
